99deeb16a2f90b4904d773020576b18d33a8a5af
u.ogur <umut.ogur13@gmail.com>
2023-06-08 17:50:19 +0300

    Course ve Instuctor Mig

--- CoreApp/Models/Course.cs
+++ CoreApp/Models/Course.cs
@@ -5,8 +5,10 @@
         public int Id { get; set; }
         public string Name { get; set; } = null!;
         public string Description { get; set; } = null!;
-
         public decimal Price { get; set; }
         public bool IsActive { get; set; }
+
+        //navigation property
+        public Instructor? Instructor { get; set; } = null;
     }
 }
